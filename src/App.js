ff2a52cd829964d9f62980a36996a5a10d5c6976
Christian larsen <cskilbredl.1993@gmail.com>
2025-03-29 23:28:45 +0100

    Update gitignore

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Internet Security Test</h1>
-        <p>Are you safe or a potential victim of the next fraud?</p>
       </header>
       <main>
         <Quiz />
